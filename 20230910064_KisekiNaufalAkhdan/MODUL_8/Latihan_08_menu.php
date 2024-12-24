da7f27a6e158058b99d4fd9eda23ff2af2c3504c
skuylivingg <20230910064@uniku.ac.id>
2024-12-24 16:11:57 +0700

    Add files via upload

---
+++ 20230910064_KisekiNaufalAkhdan/MODUL_8/Latihan_08_menu.php
@@ -0,0 +1,8 @@
+<aside class="col-md-2 p-0">
+    <nav class="nav flex-column bg-light p-3 m-0">
+        <a class="nav-link active" href="Latihan_08_a.php">Latihan 8a</a>
+        <a class="nav-link active" href="Latihan_08_b.php">Latihan 8b</a>
+        <a class="nav-link active" href="Latihan_08_c.php">Latihan 8c</a>
+        <a class="nav-link active" href="Latihan_08_d.php">Latihan 8d</a>
+    </nav>
+</aside>
